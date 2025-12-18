efa611b2bc4468caf18e04329845496addc3543a
ahmedsami019 <easami019192@gmail.com>
2025-12-18 22:54:30 +0600

    get data dynamicaly

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get('/phones', (req, res)=>{
     res.send(phones)
 })
 
+app.get('/phones/:id', (req, res)=>{
+    // const id = parseInt(req.params.id)
+    console.log(`this is the id : ${id}`);
+
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phone)
+})
+
 app.listen(port, ()=>{
     console.log(`getting data from port : ${port}`);
 })
